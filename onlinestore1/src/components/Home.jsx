fc47211b4ea0e5d8a4497c842648532b25e38c76
Sebika Nepal <sebikanepal4@gmail.com>
2023-07-06 09:46:16 +0545

    data fetch filter done

--- onlinestore1/src/components/Home.jsx
+++ onlinestore1/src/components/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
+import Products from './Products'
+
 const Home=()=> {
   return (
     <div className="hero">
 <div class="card text-bg-dark">
   <img src="./images/back.jpg" class="card-img" alt="background"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+  <div class="card-img-overlay d-flex flex-column justify-content-center">
+    <h5 class="card-title display-3 fw-bolder mb-0">NEW PRODUCTS</h5>
+    <p class="card-text lead fs-4">CHECK OUT THE PRODUCTS</p>
     <p class="card-text"><small>Last updated 3 mins ago</small></p>
   </div>
 </div>
+<Products/>
 </div>
+
  
   )
 }
